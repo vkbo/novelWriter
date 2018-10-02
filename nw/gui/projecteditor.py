39044818cab742740283f81ab9808062c9398f9e
Veronica K. B. Olsen <code@vkbo.net>
2018-10-03 00:17:25 +0200

    Added save path to project settings.

--- nw/gui/projecteditor.py
+++ nw/gui/projecteditor.py
@@ -13,7 +13,8 @@
 import logging
 import nw
 
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGroupBox, QFormLayout, QLabel, QLineEdit, QPlainTextEdit
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QFormLayout, QLabel, QLineEdit, QPlainTextEdit, QPushButton, QFileDialog
+from PyQt5.QtGui     import QIcon
 
 logger = logging.getLogger(__name__)
 
@@ -33,16 +34,29 @@ class GuiProjectEditor(QWidget):
         self.editName    = QLineEdit()
         self.editTitle   = QLineEdit()
         self.editAuthors = QPlainTextEdit()
+        self.saveBox     = QHBoxLayout()
+        self.savePath    = QLineEdit()
+        self.saveButton  = QPushButton(QIcon.fromTheme("folder"),"Browse")
+        self.saveButton.setToolTip("Browse for Save Path")
+        self.saveButton.clicked.connect(self._browseProjectPath)
+        self.saveBox.addWidget(self.savePath)
+        self.saveBox.addWidget(self.saveButton)
 
         self.mainForm.addRow("Working Name", self.editName)
         self.mainForm.addRow("Book Title",   self.editTitle)
         self.mainForm.addRow("Book Authors", self.editAuthors)
+        self.mainForm.addRow("Save Path",    self.saveBox)
 
         self.mainGroup.setLayout(self.mainForm)
         self.outerBox.addWidget(self.mainGroup)
 
         logger.debug("ProjectEditor initialisation complete")
 
+        return
+
+    def _browseProjectPath(self):
+        fileName, _ = QFileDialog.getSaveFileName(self,"Project File Name","","novelWriter File (*.nwf);;All Files (*)")
+        self.savePath.setText(fileName)
         return
 
 # END Class GuiProjectEditor
