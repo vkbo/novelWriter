3c2864f161a6fb4fd1425b56ce3bb579bc5c6bb4
Veronica K. B. Olsen <code@vkbo.net>
2019-06-11 18:46:43 +0200

    Added spell checker class

---
+++ nw/tools/spellcheck.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""novelWriter Spell Check Wrapper
+
+ novelWriter – Spell Check Wrapper
+===================================
+ Wrapper class for spell checking
+
+ File History:
+ Created: 2019-06-11 [0.1.5]
+
+"""
+
+import logging
+import enchant
+import nw
+
+logger = logging.getLogger(__name__)
+
+class NWSpellCheck():
+
+# END Class NWSpellCheck
