dbd1964ff44773e3b858a9d724054140bd648933
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2019-10-13 12:18:17 +0200

    Added export dialog class

---
+++ nw/gui/exports.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""novelWriter GUI Export Tools
+
+ novelWriter – GUI Export Tool
+================================
+ Tool for exporting project files to other formats
+
+ File History:
+ Created: 2019-10-13 [0.2.3]
+
+"""
+
+import logging
+import nw
+
+from os import path
+
+from PyQt5.QtWidgets import QDialog
+
+logger = logging.getLogger(__name__)
+
+class GuiExport(QDialog):
+
+# END Class GuiExport
