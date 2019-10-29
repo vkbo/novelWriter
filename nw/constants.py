2510e45c261bcccef2f9687e4616bfe70c4920e8
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2019-10-29 10:39:31 +0100

    Updated the missing package warnings and errors in export to link to the package site

--- nw/constants.py
+++ nw/constants.py
@@ -80,6 +80,42 @@ class nwLabels():
 
 # END Class nwLabels
 
+class nwDependencies():
+    """Python package dependencies and their reference links.
+    """
+    PACKS = {
+        "pyqt5"      : {
+            "site" : "",
+            "docs" : "",
+        },
+        "appdirs"    : {
+            "site" : "",
+            "docs" : "",
+        },
+        "lxml"       : {
+            "site" : "",
+            "docs" : "",
+        },
+        "pyenchant"  : {
+            "site" : "",
+            "docs" : "",
+        },
+        "pycountry"  : {
+            "site" : "",
+            "docs" : "",
+        },
+        "latexcodec" : {
+            "site" : "https://pypi.org/project/latexcodec/",
+            "docs" : "https://latexcodec.readthedocs.io/en/latest/",
+        },
+        "pypandoc" : {
+            "site" : "https://pypi.org/project/pypandoc/",
+            "docs" : "https://pypi.org/project/pypandoc/",
+        },
+    }
+
+# END Class nwDependencies
+
 class nwQuotes():
     """Allowed quotation marks.
     Source: https://en.wikipedia.org/wiki/Quotation_mark
