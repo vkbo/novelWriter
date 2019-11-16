dde8102cc579dcba31552deca6022107abc7dfb6
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2019-11-16 12:09:51 +0100

    Added script for plotting profiling data

---
+++ tests/profilestats.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pstats
+
+from os import path
+
+profDir = path.abspath(path.join(path.dirname(__file__),"..","prof"))
+
+print("")
+print("Profiles directory: %s" % profDir)
+print("")
+
+profMainWindows = pstats.Stats(path.join(profDir,"testMainWindows.prof"))
+profMainWindows.sort_stats("cumtime")
+profMainWindows.print_stats("nw",50)
