ba3c765ba8c1103c7f1b182b187c5e7203ca5663
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-04-20 21:50:20 +0200

    Added license text to source files

--- nw/tools/optionstate.py
+++ nw/tools/optionstate.py
@@ -9,6 +9,21 @@
  Created: 2019-10-21 [0.3.1] - Original version meant to be sub classed
  Created: 2020-02-19 [0.4.5] - Rewritten from superclass to single file tool
 
+ This file is a part of novelWriter
+ Copyright 2020, Veronica Berglyd Olsen
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see https://www.gnu.org/licenses/.
 """
 
 import logging
