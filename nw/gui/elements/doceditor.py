0f64c1fa7a692518e5ef2453aa7f4e99c6281514
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-05-26 18:40:58 +0200

    Fix the document editor and viewer margins to fit better with the document title header

--- nw/gui/elements/doceditor.py
+++ nw/gui/elements/doceditor.py
@@ -85,7 +85,8 @@ class GuiDocEditor(QTextEdit):
 
         # Document Title
         self.docTitle = GuiDocTitleBar(self, self.theProject)
-        self.docTitle.setGeometry(0,0,self.docTitle.width(),self.docTitle.height())
+        self.docTitle.setGeometry(0, 0, self.docTitle.width(), self.docTitle.height())
+        self.setViewportMargins(0, self.docTitle.height(), 0, 0)
 
         # Syntax
         self.hLight = GuiDocHighlighter(self.qDocument, self.theParent)
@@ -336,13 +337,17 @@ class GuiDocEditor(QTextEdit):
         tB = self.lineWidth()
         tW = self.width() - 2*tB
         tH = self.docTitle.height()
+        tT = tM - tH
         self.docTitle.setGeometry(tB, tB, tW, tH)
+        self.setViewportMargins(0, tH, 0, 0)
 
         docFormat = self.qDocument.rootFrame().frameFormat()
         docFormat.setLeftMargin(tM)
         docFormat.setRightMargin(tM)
-        if docFormat.topMargin() < tH:
-            docFormat.setTopMargin(tH + 2)
+        if tT > 0:
+            docFormat.setTopMargin(tT)
+        else:
+            docFormat.setTopMargin(0)
 
         # Updating root frame triggers a QTextDocument->contentsChange
         # signal, which we do not want as it re-runs the syntax
