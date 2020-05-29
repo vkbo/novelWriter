cae9705e4f0fe7f09e1736ba010aa781f2edf09d
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-05-29 23:03:04 +0200

    Files don't need the expanded flag saved to XML

--- nw/core/project.py
+++ nw/core/project.py
@@ -1572,7 +1572,6 @@ class NWItem():
         xSub = self._subPack(xPack,"type",     text=str(self.itemType.name))
         xSub = self._subPack(xPack,"class",    text=str(self.itemClass.name))
         xSub = self._subPack(xPack,"status",   text=str(self.itemStatus))
-        xSub = self._subPack(xPack,"expanded", text=str(self.isExpanded))
         if self.itemType == nwItemType.FILE:
             xSub = self._subPack(xPack,"exported",  text=str(self.isExported))
             xSub = self._subPack(xPack,"layout",    text=str(self.itemLayout.name))
@@ -1580,6 +1579,8 @@ class NWItem():
             xSub = self._subPack(xPack,"wordCount", text=str(self.wordCount), none=False)
             xSub = self._subPack(xPack,"paraCount", text=str(self.paraCount), none=False)
             xSub = self._subPack(xPack,"cursorPos", text=str(self.cursorPos), none=False)
+        else:
+            xSub = self._subPack(xPack,"expanded", text=str(self.isExpanded))
         return
 
     def unpackXML(self, xItem):
