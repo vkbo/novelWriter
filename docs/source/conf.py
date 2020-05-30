c82a3efcbf75ce28ffc242d24619e4411a602355
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-05-30 14:21:18 +0200

    Bumped version to 0.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = "Veronica Berglyd Olsen"
 # The short X.Y version
 version = "0.7.0"
 # The full version, including alpha/beta/rc tags
-release = "0.7.0rc1"
+release = "0.7.0"
 
 
 # -- General configuration ---------------------------------------------------
