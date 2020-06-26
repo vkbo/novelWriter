9609095790c0b9e873db9af02d2f10c8d5ec21a0
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-06-26 21:21:08 +0200

    Bumped version number to 0.9.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = "2018-2020, Veronica Berglyd Olsen"
 author = "Veronica Berglyd Olsen"
 
 # The short X.Y version
-version = "0.9.1"
+version = "0.9.2"
 # The full version, including alpha/beta/rc tags
-release = "0.9.1"
+release = "0.9.2"
 
 
 # -- General configuration ---------------------------------------------------
