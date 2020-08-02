4d0777b02ec791977a7dcd84f0029cf02926419b
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-08-02 21:35:45 +0200

    Remove duplicate settings from setup, and add more classifiers

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import setuptools
 
-from nw import __version__
+from nw import __version__, __url__, __docurl__, __issuesurl__, __sourceurl__
 
 with open("README.md", "r") as inFile:
-    long_description = inFile.read()
+    longDescription = inFile.read()
+
+with open("requirements.txt", "r") as inFile:
+    pkgRequirements = inFile.read().strip().splitlines()
 
 setuptools.setup(
     name = "novelWriter",
@@ -12,10 +15,10 @@ setuptools.setup(
     author = "Veronica Berglyd Olsen",
     author_email = "code@vkbo.net",
     description = "A markdown-like document editor for writing novels",
-    long_description = long_description,
+    long_description = longDescription,
     long_description_content_type = "text/markdown",
     license = "GNU General Public License v3",
-    url = "https://github.com/vkbo/novelWriter",
+    url = __url__,
     entry_points = {
         "console_scripts" : ["novelWriter-cli=nw:main"],
         "gui_scripts" :     ["novelWriter=nw:main"],
@@ -24,26 +27,24 @@ setuptools.setup(
     include_package_data = True,
     package_data = {"": ["*.conf"]},
     project_urls = {
-        "Bug Tracker": "https://github.com/vkbo/novelWriter/issues",
-        "Documentation": "https://novelwriter.readthedocs.io/",
-        "Source Code": "https://github.com/vkbo/novelWriter",
+        "Bug Tracker": __issuesurl__,
+        "Documentation": __docurl__,
+        "Source Code": __sourceurl__,
     },
     classifiers = [
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Operating System :: OS Independent",
         "Intended Audience :: End Users/Desktop",
         "Natural Language :: English",
         "Topic :: Text Editors",
     ],
     python_requires = ">=3.6",
-    install_requires = [
-        "pyqt5>=5.2.1",
-        "lxml>=4.2.0",
-        "pyenchant>=3.0.0",
-    ],
+    install_requires = pkgRequirements,
 )
