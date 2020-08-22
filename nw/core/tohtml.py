995685241a1b400ab9370bd81aa736d9ddfefbf8
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-08-22 13:20:19 +0200

    The html converter should strip these out again

--- nw/core/tohtml.py
+++ nw/core/tohtml.py
@@ -54,6 +54,8 @@ class ToHtml(Tokenizer):
             nwUnicode.U_NBSP   : nwUnicode.H_NBSP,
             nwUnicode.U_THNSP  : nwUnicode.H_THNSP,
             nwUnicode.U_THNBSP : nwUnicode.H_THNBSP,
+            nwUnicode.U_MAPOSS : nwUnicode.H_RSQUO,
+            nwUnicode.U_MAPOSD : nwUnicode.H_RDQUO,
         }
         self.revDict = {}
         self.reReplace = []
