9567e3dc4a68351bc7ecd3f9876520d979093d9c
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2020-08-23 20:56:15 +0200

    Improve regex matches for quote strings

--- nw/gui/dochighlight.py
+++ nw/gui/dochighlight.py
@@ -143,17 +143,17 @@ class GuiDocHighlighter(QSyntaxHighlighter):
         # Quoted Strings
         if self.mainConf.highlightQuotes:
             self.hRules.append((
-                "{:s}(.+?){:s}".format('"', '"'), {
+                "\\B{:s}(.*?){:s}\\B".format('"', '"'), {
                     0 : self.hStyles["dialogue1"],
                 }
             ))
             self.hRules.append((
-                "{:s}(.+?){:s}".format(*self.mainConf.fmtDoubleQuotes), {
+                "\\B{:s}(.*?){:s}\\B".format(*self.mainConf.fmtDoubleQuotes), {
                     0 : self.hStyles["dialogue2"],
                 }
             ))
             self.hRules.append((
-                "{:s}(.+?){:s}".format(*self.mainConf.fmtSingleQuotes), {
+                "\\B{:s}(.*?){:s}\\B".format(*self.mainConf.fmtSingleQuotes), {
                     0 : self.hStyles["dialogue3"],
                 }
             ))
