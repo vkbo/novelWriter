9615098d76537b1c6949541c9d48b9b43a2ddb2f
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2021-01-03 02:04:31 +0100

    Some minor improvements to the index

--- nw/core/index.py
+++ nw/core/index.py
@@ -143,16 +143,11 @@ class NWIndex():
                 logger.error(str(e))
                 return False
 
-            if "tagIndex" in theData.keys():
-                self.tagIndex = theData["tagIndex"]
-            if "refIndex" in theData.keys():
-                self.refIndex = theData["refIndex"]
-            if "novelIndex" in theData.keys():
-                self.novelIndex = theData["novelIndex"]
-            if "noteIndex" in theData.keys():
-                self.noteIndex = theData["noteIndex"]
-            if "textCounts" in theData.keys():
-                self.textCounts = theData["textCounts"]
+            self.tagIndex   = theData.get("tagIndex", {})
+            self.refIndex   = theData.get("refIndex", {})
+            self.novelIndex = theData.get("novelIndex", {})
+            self.noteIndex  = theData.get("noteIndex", {})
+            self.textCounts = theData.get("textCounts", {})
 
             nowTime = round(time())
             self.timeNovel = nowTime
@@ -594,8 +589,7 @@ class NWIndex():
             return theRefs
 
         for refTitle in self.refIndex[tHandle]:
-            theTags = self.refIndex[tHandle][refTitle].get("tags", [])
-            for aTag in theTags:
+            for aTag in self.refIndex[tHandle][refTitle].get("tags", []):
                 if len(aTag) == 3 and (sTitle is None or sTitle == refTitle):
                     theRefs[aTag[1]].append(aTag[2])
 
