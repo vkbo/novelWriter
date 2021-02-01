dd180402325a232c580d0068caad694f0b9929f1
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2021-02-01 21:52:51 +0100

    Fix some source file headers

--- nw/gui/noveltree.py
+++ nw/gui/noveltree.py
@@ -1,28 +1,27 @@
 # -*- coding: utf-8 -*-
-"""novelWriter GUI Novel Tree
+"""
+novelWriter – GUI Novel Tree
+============================
+GUI classe for the main window novel tree
 
- novelWriter – GUI Novel Tree
-==============================
- Class holding the project's novel files tree view
+File History:
+Created: 2020-12-20 [1.1a0]
 
- File History:
- Created: 2020-12-20 [1.1a0]
+This file is a part of novelWriter
+Copyright 2018–2020, Veronica Berglyd Olsen
 
- This file is a part of novelWriter
- Copyright 2018–2020, Veronica Berglyd Olsen
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
 
- This program is distributed in the hope that it will be useful, but
- WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program. If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 
 import nw
