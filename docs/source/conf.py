27de8138f58e53e619be38f2730e663a6822ced6
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2021-02-10 23:00:40 +0100

    Bumped version to 1.3 alpha0 for next dev cycle

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = "2018–2021, Veronica Berglyd Olsen"
 author = "Veronica Berglyd Olsen"
 
 # The short X.Y version
-version = "1.2"
+version = "1.3"
 # The full version, including alpha/beta/rc tags
-release = "1.2-alpha0"
+release = "1.3-alpha0"
 
 
 # -- General configuration ---------------------------------------------------
