c23ec06ce6f9d5b8891fc8f9ac217e123a9f7099
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2021-03-28 17:54:06 +0200

    Bumped version to 1.3b1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = "Veronica Berglyd Olsen"
 # The short X.Y version
 version = "1.3"
 # The full version, including alpha/beta/rc tags
-release = "1.3-alpha0"
+release = "1.3-beta1"
 
 
 # -- General configuration ---------------------------------------------------
