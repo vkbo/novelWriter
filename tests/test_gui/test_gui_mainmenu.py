2314777b4bf0a6a57d6517f1a66671d17f3767c4
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2021-05-23 15:04:26 +0200

    Add test coverage

--- tests/test_gui/test_gui_mainmenu.py
+++ tests/test_gui/test_gui_mainmenu.py
@@ -236,6 +236,47 @@ def testGuiMenu_EditFormat(qtbot, monkeypatch, nwGUI, nwLipsum):
         "Also text with “double” quotes which are “less tricky”.\n\n"
     )
 
+    # Remove in-paragraph line breaks
+    nwGUI.docEditor.setText((
+        "### New Text\n\n"
+        "@char: Someone\n"
+        "@location: Somewhere\n\n"
+        "% Some comment ...\n\n"
+        "Here is some text\non multiple\nlines.\n\n"
+        "With another paragraph\nhere."
+    ))
+    nwGUI.mainMenu.aFmtRmBreaks.activate(QAction.Trigger)
+    assert nwGUI.docEditor.getText() == (
+        "### New Text\n\n"
+        "@char: Someone\n"
+        "@location: Somewhere\n\n"
+        "% Some comment ...\n\n"
+        "Here is some text on multiple lines.\n\n"
+        "With another paragraph here.\n"
+    )
+
+    nwGUI.docEditor.setText((
+        "### New Text\n\n"
+        "@char: Someone\n"
+        "@location: Somewhere\n\n"
+        "% Some comment ...\n\n"
+        "Here is some text\non multiple\nlines.\n\n"
+        "With another paragraph\nhere."
+    ))
+    theCursor = nwGUI.docEditor.textCursor()
+    theCursor.setPosition(74)
+    theCursor.movePosition(QTextCursor.Right, QTextCursor.KeepAnchor, 29)
+    nwGUI.docEditor.setTextCursor(theCursor)
+    nwGUI.mainMenu.aFmtRmBreaks.activate(QAction.Trigger)
+    assert nwGUI.docEditor.getText() == (
+        "### New Text\n\n"
+        "@char: Someone\n"
+        "@location: Somewhere\n\n"
+        "% Some comment ...\n\n"
+        "Here is some text on multiple lines.\n\n"
+        "With another paragraph\nhere."
+    )
+
     # Test Invalid Document Action
     assert not nwGUI.docEditor.docAction(nwDocAction.NO_ACTION)
 
