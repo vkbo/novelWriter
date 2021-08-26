f143e3d9c146aa9e7f63c1544b7342f87d170d08
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2021-08-26 21:06:03 +0200

    Bump version to 1.5b2 and update changelog and release notes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ author = "Veronica Berglyd Olsen"
 # The short X.Y version
 version = "1.5"
 # The full version, including alpha/beta/rc tags
-release = "1.5-beta1"
+release = "1.5-beta2"
 
 # -- General Configuration ---------------------------------------------------
 
