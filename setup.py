b0909dc456da6e4ea0223f6e3e38c68df34af527
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2021-10-24 15:32:32 +0200

    Drop .pro file and generate lupdate command on the fly

--- setup.py
+++ setup.py
@@ -302,7 +302,7 @@ def buildQtI18n():
 #  Qt Linguist TS Builder (qtlupdate)
 ##
 
-def buildQtI18nTS():
+def buildQtI18nTS(sysArgs):
     """Build the lang.ts files for Qt Linguist.
     """
     print("")
@@ -310,8 +310,57 @@ def buildQtI18nTS():
     print("=============================")
     print("")
 
+    srcList = [os.path.join("i18n", "qtbase.py")]
+    for nRoot, _, nFiles in os.walk("novelwriter"):
+        if os.path.isdir(nRoot):
+            for aFile in nFiles:
+                aPath = os.path.join(nRoot, aFile)
+                if os.path.isfile(aPath) and aFile.endswith(".py"):
+                    srcList.append(aPath)
+
+    print("Scanning Source Tree:")
+    for aSource in srcList:
+        print(aSource)
+    print("")
+
+    tsList = []
+    if len(sysArgs) >= 2:
+        for anArg in sysArgs[1:]:
+            if not (anArg.startswith("i18n") and anArg.endswith(".ts")):
+                continue
+
+            fName = os.path.basename(anArg)
+            if not fName.startswith("nw_") and len(fName) > 6:
+                print("Skipping non-novelWriter TS file %s" % fName)
+                continue
+
+            if os.path.isfile(anArg):
+                tsList.append(anArg)
+            elif os.path.exists(anArg):
+                pass
+            else:  # Create an empty new language file
+                lCode = fName[3:-3]
+                writeFile(anArg, (
+                    "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+                    "<!DOCTYPE TS>\n"
+                    f"<TS version=\"2.0\" language=\"{lCode}\" sourcelanguage=\"en_GB\"/>\n"
+                ))
+                tsList.append(anArg)
+
+    else:
+        for aFile in os.listdir("i18n"):
+            aPath = os.path.join("i18n", aFile)
+            if os.path.isfile(aPath) and aFile.endswith(".ts"):
+                tsList.append(aPath)
+
+    print("TS Files to Update:")
+    for aTS in tsList:
+        print(aTS)
+    print("")
+
+    print("Updating Language Files:")
     try:
-        subprocess.call(["pylupdate5", "-verbose", "-noobsolete", "novelWriter.pro"])
+        subprocess.call(["pylupdate5", "-verbose", "-noobsolete", *srcList, "-ts", *tsList])
     except Exception as e:
         print("PyQt5 Linguist tools seem to be missing")
         print("On Debian/Ubuntu, install: qttools5-dev-tools pyqt5-dev-tools")
@@ -1440,9 +1489,10 @@ if __name__ == "__main__":
         "Additional Builds:",
         "",
         "    manual         Build the help documentation as PDF (requires LaTeX).",
-        "    qtlupdate      Update the translation files for internationalisation.",
-        "    qtlrelease     Build the language files for internationalisation.",
         "    sample         Build the sample project zip file and add it to assets.",
+        "    qtlupdate      Update the translation files for internationalisation.",
+        "                   To update specific TS files, list them after the command.",
+        "    qtlrelease     Build the language files for internationalisation.",
         "",
         "Python Packaging:",
         "",
@@ -1519,7 +1569,8 @@ if __name__ == "__main__":
 
     if "qtlupdate" in sys.argv:
         sys.argv.remove("qtlupdate")
-        buildQtI18nTS()
+        buildQtI18nTS(sys.argv)
+        sys.exit(0)  # Don't continue execution
 
     if "sample" in sys.argv:
         sys.argv.remove("sample")
