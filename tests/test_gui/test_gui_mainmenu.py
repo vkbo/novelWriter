dc401278fb93711e518f918b3e757f88b19796f4
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2022-10-29 01:05:41 +0200

    Update tests

--- tests/test_gui/test_gui_mainmenu.py
+++ tests/test_gui/test_gui_mainmenu.py
@@ -116,6 +116,16 @@ def testGuiMenu_EditFormat(qtbot, monkeypatch, nwGUI, nwLipsum):
     fmtStr = "#### Pellentesque nec erat ut nulla posuere commodo."
     assert nwGUI.docEditor.getText()[39:91] == fmtStr
 
+    # Title Format
+    nwGUI.mainMenu.aFmtTitle.activate(QAction.Trigger)
+    fmtStr = "#! Pellentesque nec erat ut nulla posuere commodo."
+    assert nwGUI.docEditor.getText()[39:89] == fmtStr
+
+    # Unnumbered Chapter
+    nwGUI.mainMenu.aFmtUnNum.activate(QAction.Trigger)
+    fmtStr = "##! Pellentesque nec erat ut nulla posuere commodo."
+    assert nwGUI.docEditor.getText()[39:90] == fmtStr
+
     # Clear Format
     nwGUI.mainMenu.aFmtNoFormat.activate(QAction.Trigger)
     assert nwGUI.docEditor.getText()[39:86] == cleanText
@@ -312,10 +322,6 @@ def testGuiMenu_EditFormat(qtbot, monkeypatch, nwGUI, nwLipsum):
 
     # Cannot Format Tag
     assert nwGUI.docEditor.setCursorPosition(17)
-    assert not nwGUI.docEditor._formatBlock(nwDocAction.BLOCK_TXT)
-
-    # Cannot Format Empty Line
-    assert nwGUI.docEditor.setCursorPosition(13)
     assert not nwGUI.docEditor._formatBlock(nwDocAction.BLOCK_TXT)
 
     # Invalid Action
