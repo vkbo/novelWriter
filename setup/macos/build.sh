48a7929254a3e67719ab88d9b1001cd019644946
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2024-04-20 18:28:57 +0200

    Escape forwarding of arguments in MacoS build script

--- setup/macos/build.sh
+++ setup/macos/build.sh
@@ -144,7 +144,7 @@ echo "Creating entry script ..."
 cat > novelWriter.app/Contents/MacOS/novelWriter << EOF
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$DIR/../Resources/bin/python -sE $DIR/../Resources/novelWriter/novelWriter.py $@
+$DIR/../Resources/bin/python -sE $DIR/../Resources/novelWriter/novelWriter.py \$\@
 EOF
 
 # Make it executable
