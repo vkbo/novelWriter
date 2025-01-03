a819e646a51f6dc0139e721c6cf426ccf3f98f16
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2025-01-03 12:56:13 +0100

    Remove search/replace duplication

--- novelwriter/formats/toqdoc.py
+++ novelwriter/formats/toqdoc.py
@@ -6,7 +6,7 @@ File History:
 Created: 2024-05-21 [2.5b1] ToQTextDocument
 
 This file is a part of novelWriter
-Copyright (C) 2024 Veronica Berglyd Olsen and novelWriter contributors and novelWriter contributors
+Copyright (C) 2024 Veronica Berglyd Olsen and novelWriter contributors
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
