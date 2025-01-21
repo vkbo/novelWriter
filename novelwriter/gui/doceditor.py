53dfdb3a2e532122389fd593455345595b6b7b82
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2025-01-22 00:48:59 +0100

    Fix potential undefined variable in editor

--- novelwriter/gui/doceditor.py
+++ novelwriter/gui/doceditor.py
@@ -1104,10 +1104,10 @@ class GuiDocEditor(QPlainTextEdit):
             else:
                 self._completer.setVisible(False)
 
-        if self._doReplace and added == 1:
-            cursor = self.textCursor()
-            if self._autoReplace.process(text, cursor):
-                self._qDocument.syntaxHighlighter.rehighlightBlock(cursor.block())
+            if self._doReplace and added == 1:
+                cursor = self.textCursor()
+                if self._autoReplace.process(text, cursor):
+                    self._qDocument.syntaxHighlighter.rehighlightBlock(cursor.block())
 
         return
 
