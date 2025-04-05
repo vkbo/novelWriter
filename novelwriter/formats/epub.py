21684e46cce9dd0351ad4c01bcee92fc3afb1f5a
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2025-04-05 15:19:27 +0200

    Add meta info to epub file

--- novelwriter/formats/epub.py
+++ novelwriter/formats/epub.py
@@ -24,13 +24,33 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 from __future__ import annotations
 
 import logging
+import xml.etree.ElementTree as ET
 
 from pathlib import Path
+from zipfile import ZIP_DEFLATED, ZipFile
 
+from novelwriter.common import xmlElement, xmlSubElem
 from novelwriter.core.project import NWProject
 from novelwriter.formats.tokenizer import Tokenizer
 
 logger = logging.getLogger(__name__)
+
+X_MIME = "application/epub+zip"
+
+# Main XML NameSpaces
+XML_NS = {
+    "dc": "http://purl.org/dc/elements/1.1/",
+}
+for ns, uri in XML_NS.items():
+    ET.register_namespace(ns, uri)
+
+
+def _mkTag(ns: str, tag: str) -> str:
+    """Assemble namespace and tag name."""
+    if uri := XML_NS.get(ns, ""):
+        return f"{{{uri}}}{tag}"
+    logger.warning("Missing xml namespace '%s'", ns)
+    return tag
 
 
 class ToEPub(Tokenizer):
@@ -50,4 +70,23 @@ class ToEPub(Tokenizer):
         return
 
     def saveDocument(self, path: Path) -> None:
+        """Save the data to a .epub file."""
+
+        xContainer = xmlElement("container", attrib={
+            "xmlns": "urn:oasis:names:tc:opendocument:xmlns:container",
+            "version": "1.0",
+        })
+        xRootFiles = xmlSubElem(xContainer, "rootfiles")
+        xmlSubElem(xRootFiles, "rootfile", attrib={
+            "full-path": "OEBPS/package.opf",
+            "media-type": "application/oebps-package+xml",
+        })
+
+        def xmlToZip(name: str, root: ET.Element, zipObj: ZipFile) -> None:
+            zipObj.writestr(name, ET.tostring(root, encoding="utf-8", xml_declaration=True))
+
+        with ZipFile(path, mode="w", compression=ZIP_DEFLATED, compresslevel=3) as outZip:
+            outZip.writestr("mimetype", X_MIME, compress_type=None, compresslevel=None)
+            xmlToZip("META-INF/container.xml", xContainer, outZip)
+
         return
